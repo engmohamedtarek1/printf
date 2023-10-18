6f3506dea7e4e67f9454c51ae9f91833443447c5
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2023-10-18 16:13:00 +0300

    Update _printf.c 2

--- _printf.c
+++ _printf.c
@@ -1,60 +1,47 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+#include <limits.h>
+#include <stdio.h>
 
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
-
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-int printed_chars = 0, printed = 0, i;
-int width, flags, precision, buff_ind = 0, size;
-va_list list;
-char buffer[BUFF_SIZE];
+	int (*pointer_function)(va_list, flags_t *);
+	const char *pointer;
+	va_list args;
+	flags_t flags = {0, 0, 0};
 
-if (format == NULL) {
-    return (-1);
-}
-va_start(list, format);
-for (i = 0; && format[i] != '\0' && format; ++i) {
-    if (format[i] != '%') {
-        buffer[buff_ind++] = format[i];
-        if (buff_ind == BUFF_SIZE) {
-            print_buffer(buffer, &buff_ind);
-        }
-        ++printed_chars;
-    }
-    else {
-        print_buffer(buffer, &buff_ind);
-        precision = get_precision(format, &i, list);
-        width = get_width(format, &i, list);
-        flags = get_flags(format, &i);
-        size = get_size(format, &i);
-        ++i;
-        printed = handle_print(format, &i, list, buffer,
-        flags, width, precision, size);
-        if (printed == -1) {
-            return (-1);
-        }
-        printed_chars = printed_chars + printed;
-    }
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
+	register int cnt = 0;
 
-/**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buff_ind: Index at which to add next char, represents the length.
-*/
-
-void print_buffer(char buffer[], int *buff_ind) {
-if (*buff_ind > 0) {
-    write(1, &buffer[0], *buff_ind);
-}
-*buff_ind = 0;
+	va_start(args, format);
+	if (format[1] == ' ' && format[0] == '%' && !format[2])
+		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	for (pointer = format; *pointer; pointer++)
+	{
+		if (*pointer == '%')
+		{
+			++pointer;
+			if (*pointer == '%')
+			{
+				cnt = cnt + _putchar('%');
+				continue;
+			}
+			while (get_flag(*pointer, &flags))
+				pointer++;
+			pointer_function = get_print(*pointer);
+			cnt = cnt + (pointer_function ? pointer_function(args, &flags) : _printf("%%%c", *pointer));
+		} else
+			cnt = cnt + _putchar(*pointer);
+	}
+	_putchar(-1);
+	va_end(args);
+	return (cnt);
 }
